9480c403e07773634aee59aa5158c4e8f73d456d
patjoly <patjoly2@gmail.com>
2022-10-25 22:43:15 -0400

    increment $VERSION after v1.06 release

--- lib/Geo/Gpx.pm
+++ lib/Geo/Gpx.pm
@@ -3,7 +3,7 @@ package Geo::Gpx;
 use warnings;
 use strict;
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 use Carp;
 use DateTime::Format::ISO8601;
@@ -1398,7 +1398,7 @@ Please visit the project page at: L<https://github.com/patjoly/geo-gpx>.
 
 =head1 VERSION
 
-1.06
+1.07
 
 =head1 LICENSE AND COPYRIGHT
 
--- lib/Geo/Gpx/Point.pm
+++ lib/Geo/Gpx/Point.pm
@@ -2,7 +2,7 @@ package Geo::Gpx::Point;
 use strict;
 use warnings;
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 =encoding utf8
 
@@ -280,7 +280,7 @@ Patrick Joly C<< <patjol@cpan.org> >>.
 
 =head1 VERSION
 
-1.06
+1.07
 
 =head1 SEE ALSO
 
