94f72949527e192a727617633878d2c4fbc36838
patjoly <patjoly2@gmail.com>
2022-12-06 21:15:48 -0500

    increment $VERSION after v1.09 release

--- lib/Geo/Gpx.pm
+++ lib/Geo/Gpx.pm
@@ -3,7 +3,7 @@ package Geo::Gpx;
 use warnings;
 use strict;
 
-our $VERSION = '1.09';
+our $VERSION = '1.10';
 
 use Carp;
 use DateTime::Format::ISO8601;
@@ -1298,7 +1298,7 @@ Please visit the project page at: L<https://github.com/patjoly/geo-gpx>.
 
 =head1 VERSION
 
-1.09
+1.10
 
 =head1 LICENSE AND COPYRIGHT
 
